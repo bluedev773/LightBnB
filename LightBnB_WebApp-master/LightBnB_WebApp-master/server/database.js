d7291914f4c3de2d436161581fbcafe7fc9a1c26
Matthew Litvinov <matthew.litvinov@gmail.com>
2022-01-08 15:09:24 -0600

    complete getAllReservations function

--- LightBnB_WebApp-master/LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/LightBnB_WebApp-master/server/database.js
@@ -55,7 +55,7 @@ const addUser =  function(user) {
   const email = user.email;
   const password = user.password;
 
-  pool
+  return pool
    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [name, email, password])
    .then((result) => result.rows)
    .catch((err) => {
@@ -72,7 +72,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message)
+    })
 }
 exports.getAllReservations = getAllReservations;
 
